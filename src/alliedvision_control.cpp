1a883017ec450ca26eaeebc3a8015a80fd79c41d
Luke Chang <luke@boxfish.nz>
2024-03-06 17:17:04 +1300

    working progress

--- src/alliedvision_control.cpp
+++ src/alliedvision_control.cpp
@@ -1,11 +1,16 @@
 #include "Heartbeater.hpp"
+#include "opencv2/core/core.hpp"
 #include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/distortion_models.hpp"
 #include "sensor_msgs/msg/camera_info.hpp"
 #include "sensor_msgs/msg/image.hpp"
 #include "VmbCPP/VmbCPP.h"
 
+#include <chrono>
 #include <iostream>
 #include <sstream>
+
+using namespace std::chrono_literals;
 
 class AlliedVisionControl : public rclcpp::Node
 {
@@ -27,9 +32,20 @@ class AlliedVisionControl : public rclcpp::Node
           heartbeater(this),
           vmb_system(VmbCPP::VmbSystem::GetInstance())
     {
+        // Quality of Service
         rclcpp::QoS qos(2);
         qos.reliable();
         qos.durability_volatile();
+
+        // Image data does not keep samples in the queue.
+        rclcpp::QoS img_qos(0);
+        img_qos.history(RMW_QOS_POLICY_HISTORY_KEEP_LAST);
+        img_qos.reliability(RMW_QOS_POLICY_RELIABILITY_RELIABLE);
+        img_qos.durability(RMW_QOS_POLICY_DURABILITY_VOLATILE);
+        img_qos.deadline(rclcpp::Duration(2, 0));
+        img_qos.lifespan(rclcpp::Duration(2, 0));
+        img_qos.liveliness(RMW_QOS_POLICY_LIVELINESS_AUTOMATIC);
+        img_qos.liveliness_lease_duration(rclcpp::Duration(6, 0));
 
         // Set parameters
         this->params.camera_id_top = declare_parameter<std::string>("camera_id_top", "DEV_1AB22C020237");
@@ -42,8 +58,11 @@ class AlliedVisionControl : public rclcpp::Node
         this->params.save_top = declare_parameter<bool>("save_top_images", true);
 
         // Initialize topics and services
+        this->top_img_publisher = create_publisher<sensor_msgs::msg::Image>("raw_top_allied_img", img_qos);
+        this->top_cam_info_publisher = create_publisher<sensor_msgs::msg::CameraInfo>("raw_top_camera_info", img_qos);
 
         // Initialize timer
+        this->top_cam_timer = create_wall_timer(5ms, std::bind(&AlliedVisionControl::topImgPublisher, this));
 
         printAllCameraInfo();
 
@@ -70,6 +89,10 @@ class AlliedVisionControl : public rclcpp::Node
 
     // Timers
     rclcpp::TimerBase::SharedPtr top_cam_timer;
+
+    void topImgPublisher()
+    {
+    }
 
     /** Print information for all Allied Vision cameras.
      * Copied from VmbCPP example code: ${VmbCPP_DIR}/api/examples/VmbCPP/ListCameras/ListCameras.cpp
@@ -189,6 +212,68 @@ class AlliedVisionControl : public rclcpp::Node
            << "    @ TransportLayer ID  : " << transport_layer_id << std::endl
            << "    @ TransportLayer Path: " << transport_layer_path << std::endl;
         return ss.str();
+    }
+
+    /** Create CameraInfo object from an OpenCV calibration file.
+     * Calibration parameters: https://docs.ros.org/en/noetic/api/sensor_msgs/html/msg/CameraInfo.html
+     */
+    void copyIntrinsics(const cv::FileStorage& fs, sensor_msgs::msg::CameraInfo& info)
+    {
+        cv::Mat camera_matrix, distortion_coefficients;
+        double image_width, image_height;
+
+        fs["image_width"] >> image_width;
+        fs["image_height"] >> image_height;
+        fs["camera_matrix"] >> camera_matrix;
+        fs["distortion_coefficients"] >> distortion_coefficients;
+
+        info.height = image_height;
+        info.width = image_width;
+        info.distortion_model = sensor_msgs::distortion_models::RATIONAL_POLYNOMIAL;
+
+        // Distortion parameters
+        const int distortion_coef_size = distortion_coefficients.size().height * distortion_coefficients.size().width;
+        // Rational polynomial support 8 parameters: [k1, k2, p1, p2, k3, k4, k5, k6]
+        // https://www.ros.org/reps/rep-0104.html#alternate-distortion-models
+        for(int i = 0; (i < distortion_coef_size && i < 8); i++)
+        {
+            info.d.push_back(distortion_coefficients.at<double>(0, i));
+        }
+
+        // Intrinsic camera matrix
+        for(int row = 0; row < camera_matrix.size().height; row++)
+        {
+            for(int col = 0; col < camera_matrix.size().width; col++)
+            {
+                info.k[(camera_matrix.size().width * row) + col] = camera_matrix.at<double>(row, col);
+            }
+        }
+
+        // Rectification matrix (stereo cameras only)
+        info.r[0] = 1.0;
+        info.r[4] = 1.0;
+        info.r[8] = 1.0;
+
+        // Projection/camera matrix
+        // Assume 0 translation between cameras. This is definitely wrong, use a stereo calib file to override.
+        info.p[0] = info.k[0];
+        info.p[1] = info.k[1];
+        info.p[2] = info.k[2];
+        info.p[3] = 0.0;
+        info.p[4] = info.k[3];
+        info.p[5] = info.k[4];
+        info.p[6] = info.k[5];
+        info.p[7] = 0.0;
+        info.p[8] = info.k[6];
+        info.p[9] = info.k[7];
+        info.p[10] = info.k[8];
+        info.p[11] = 0.0;
+    }
+
+    /** Update CameraInfo object from a stereo calibration file.
+     */
+    void copyStereoExtrinsics(const cv::FileStorage& fs, sensor_msgs::msg::CameraInfo& info, std::string cam_location)
+    {
     }
 };
 
